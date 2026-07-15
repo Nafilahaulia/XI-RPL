c12316b15866306f20e5647c24d6aa489bd4a3c3
aulianafilah718@gmail.com <aulianafilah718@gmail.com>
2026-07-15 14:03:23 +0700

    first commit

---
+++ Video/VID 2/VID 2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BelajarCSharp
+    {
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
---
+++ Video/VID 5/VID 5/Program.cs
@@ -0,0 +1,36 @@
+﻿// Materi Perulangan For, While, dan Foreach
+
+//Perulangan digunakan untuk menjalanjan perintah berulang otomatis
+// dengan perulangan, program dapat menghemat penulisan Code
+
+
+
+//FOR
+//Digunakan ketika jumlah perulangan sudah diketahui
+
+//For digunakan untuk perulangan dengan jumlah tertentu
+
+//Isi (Parameter) dari For Loop
+//int i = 1      <- Nilai awal perulangan
+//i <= 5         <- Kondisi selama perulangan berjalan
+//i++            <- Menambah nilai i setiap perulangan
+
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine("Perulangan Ke - " + i); // Artinya code diulang selama variabel i kurang dari atau sama dengan 5
+}
+
+
+
+//2. While Loop
+//digunakan ketika jumlah perulangan tidak diketahui, dan perulangan akan terus berjalan selama kondisi bernilai true
+
+int angka = 1;
+
+while (angka <= 5) //While akan berjalan selama kondisi bernilai true
+{
+    Console.WriteLine("Perulangan Ke - " + angka);
+    angka++; // Increment wajib, agar perulangan tidak berjalan terus menerus
+             // Jika increment tidak ditambahkan, program dapat mengalami infinite loop (perulangan tanpa henti)
+}
