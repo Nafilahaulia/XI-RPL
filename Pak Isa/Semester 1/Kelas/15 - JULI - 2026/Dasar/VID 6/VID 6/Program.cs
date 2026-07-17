3ef0b47d3f0f28294863c5d1772cb338a5add249
Nafilahaulia <aulianafilah718@gmail.com>
2026-07-17 22:32:14 +0700

    Add files via upload

---
+++ Pak Isa/Semester 1/Kelas/15 - JULI - 2026/Dasar/VID 6/VID 6/Program.cs
@@ -0,0 +1,52 @@
+﻿// Materi Array dan Koleksi Dasar
+
+// Pada Materi ini kita akan mempelajari:
+// Array
+// List
+// serta menyimpan banyak data dalam satu tempat
+
+// 1. Array
+// Digunakan untuk menyimpan banya data dalam satu variabel
+// Array biasanya digunakan ketika data memiliki tipe yang sama
+
+// Array dapat menyimpan banyak data dengan tipe data yang sama
+
+string[] namaBuah = { "Apel", "Jeruk", "Mangga", "Pisang" }; // Array string untuk menyimpan beberapa nama buah
+
+// penjelasan struktur array
+// string[]  <- tanda [] menunjukkan bahwa variabel adalah array
+
+// Mengambil data dari array
+//Console.WriteLine(namaBuah[0]); // hasilnya akan menampilkan apel (isi pertama array) karena index array dimulai dari angka 0
+//Console.WriteLine(namaBuah[2]);
+
+// Array juga sering digunakan bersama dengan perulangan foreach
+
+foreach (string buah in namaBuah)
+{
+    Console.WriteLine(buah);
+}
+
+// Dengan pengulangan, kita dapat menghemat penulisan array tanpa harus menulis satu persatu
+
+
+// 2. Koleksi Dasar: List
+// adalah koleksi data yang lebih fleksibel dibandingkan array
+
+List<string> namaSiswa = new List<string>(); // Membuat list kosong untuk menyimpan nama siswa
+
+// Menambahkan data ke dalam list
+namaSiswa.Add("Budi"); // .Add digunakan untuk menambahkan data ke dalam list
+namaSiswa.Add("Anton");
+namaSiswa.Add("Siti");
+
+// Coba kita tampilkan hasil dari list
+foreach (string siswa in namaSiswa)
+{
+    Console.WriteLine(siswa);
+}
+
+// Bedanya dengan array, ukuran dari list lebih fleksibel dibandingkan dengan array yang memiliki ukuran tetap
+// dan cocok untuk data yang sering berubah-ubah (Dinamis)
+
+//Dengan ini kita sudah mempelajari array dan koleksi dasar untuk menyimpan data di satu tempat.
---
+++ Pak Isa/Semester 1/Kelas/15 - JULI - 2026/Dasar/VID 7/VID 7/Program.cs
@@ -0,0 +1,55 @@
+﻿// Method / fungsi sederhana
+
+// Pada materi ini kita akan mempelajari:
+// 1. Method / fungsi
+// 2. Parameter
+// 3. Return value
+
+
+// 1. Method / fungsi
+// adalah kumpulan code yang digunakan untuk menjalankan tugas tertentu
+// dapat membantu program menjadi:
+// - lebih rapih
+// - lebih terstruktur
+// - dan dapat digunakan kembali (Reuse)
+
+//      void karena method tidak mengembalikan nilai (tanpa return Value)
+static void Salam()
+{
+    Console.WriteLine("Hello World");
+}
+
+// Memanggil method tanpa return value dan tanpa parameter
+Salam();
+
+
+// 2. Method dengan Parameter
+// Parameter digunakan untuk mengirim data ke method
+
+static void Sapa(string nama) //parameter bertipe string
+{
+    Console.WriteLine("Halo " + nama); //Variabel Parameter dimasukkan ke dalam method
+}
+
+// Memanggil Method dengan parameter sesuai dengan tipe data parameter pada method yang kita buat sebelumnya
+Sapa("Budi"); //Memanggil method Sapa dengan parameter "Budi")
+
+
+
+// 3. Method dengan Return Value
+// Method juga dapat mengembalikan nilai (Return Value)
+
+//      int dan bukan void karena method memiliki return value
+static int Tambah(int a, int b)  // Method akan menghasilkan nilai bertipe int
+{
+    return a + b; // return digunakan untuk mengembalikan hasil
+}
+
+// Memanggil method dengan return value
+Console.WriteLine(Tambah(5, 7));
+
+// Method digunakan untuk menjalankan tugas tertentu
+// Parameter digunakan untuk menerima data
+// return untuk mengembalikan nilai
+
+// dengan ini kita sudah mempelajari Method / Fungsi dasar.
