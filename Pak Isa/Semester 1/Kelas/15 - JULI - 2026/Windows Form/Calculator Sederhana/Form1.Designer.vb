96d24f3a8ee4db1b1de5298b7063b9b359e35bc0
Nafilahaulia <aulianafilah718@gmail.com>
2026-07-24 22:49:01 +0700

    Add files via upload

---
+++ Pak Isa/Semester 1/Kelas/15 - JULI - 2026/Windows Form/Calculator Sederhana/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.txtAngka1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtAngka2 = New System.Windows.Forms.TextBox()
+        Me.lblHasil = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnKurang = New System.Windows.Forms.Button()
+        Me.btnKali = New System.Windows.Forms.Button()
+        Me.btnBagi = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(12, 64)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(55, 17)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "Angka 1"
+        '
+        'btnTambah
+        '
+        Me.btnTambah.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTambah.Location = New System.Drawing.Point(15, 259)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(358, 23)
+        Me.btnTambah.TabIndex = 1
+        Me.btnTambah.Text = "Tambah"
+        Me.btnTambah.UseVisualStyleBackColor = True
+        '
+        'txtAngka1
+        '
+        Me.txtAngka1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAngka1.Location = New System.Drawing.Point(12, 84)
+        Me.txtAngka1.Name = "txtAngka1"
+        Me.txtAngka1.Size = New System.Drawing.Size(358, 25)
+        Me.txtAngka1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 123)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Angka 2"
+        '
+        'txtAngka2
+        '
+        Me.txtAngka2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAngka2.Location = New System.Drawing.Point(15, 143)
+        Me.txtAngka2.Name = "txtAngka2"
+        Me.txtAngka2.Size = New System.Drawing.Size(358, 25)
+        Me.txtAngka2.TabIndex = 4
+        '
+        'lblHasil
+        '
+        Me.lblHasil.AutoSize = True
+        Me.lblHasil.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHasil.Location = New System.Drawing.Point(12, 181)
+        Me.lblHasil.Name = "lblHasil"
+        Me.lblHasil.Size = New System.Drawing.Size(51, 17)
+        Me.lblHasil.TabIndex = 5
+        Me.lblHasil.Text = "Hasil : "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(175, 239)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 17)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Aksi"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(94, 21)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(201, 25)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Calculator Sederhana"
+        '
+        'btnKurang
+        '
+        Me.btnKurang.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnKurang.Location = New System.Drawing.Point(15, 288)
+        Me.btnKurang.Name = "btnKurang"
+        Me.btnKurang.Size = New System.Drawing.Size(358, 23)
+        Me.btnKurang.TabIndex = 8
+        Me.btnKurang.Text = "Kurang"
+        Me.btnKurang.UseVisualStyleBackColor = True
+        '
+        'btnKali
+        '
+        Me.btnKali.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnKali.Location = New System.Drawing.Point(15, 317)
+        Me.btnKali.Name = "btnKali"
+        Me.btnKali.Size = New System.Drawing.Size(358, 23)
+        Me.btnKali.TabIndex = 9
+        Me.btnKali.Text = "Kali"
+        Me.btnKali.UseVisualStyleBackColor = True
+        '
+        'btnBagi
+        '
+        Me.btnBagi.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBagi.Location = New System.Drawing.Point(15, 346)
+        Me.btnBagi.Name = "btnBagi"
+        Me.btnBagi.Size = New System.Drawing.Size(358, 23)
+        Me.btnBagi.TabIndex = 10
+        Me.btnBagi.Text = "Bagi"
+        Me.btnBagi.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(385, 381)
+        Me.Controls.Add(Me.btnBagi)
+        Me.Controls.Add(Me.btnKali)
+        Me.Controls.Add(Me.btnKurang)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblHasil)
+        Me.Controls.Add(Me.txtAngka2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtAngka1)
+        Me.Controls.Add(Me.btnTambah)
+        Me.Controls.Add(Me.label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents label1 As Label
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents txtAngka1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtAngka2 As TextBox
+    Friend WithEvents lblHasil As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnKurang As Button
+    Friend WithEvents btnKali As Button
+    Friend WithEvents btnBagi As Button
+End Class
---
+++ Pak Isa/Semester 1/Kelas/15 - JULI - 2026/Windows Form/Calculator Sederhana/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim angka1 As Integer = Convert.ToInt32(txtAngka1.Text)
+        Dim angka2 As Integer = Convert.ToInt32(txtAngka2.Text)
+
+        Dim hasil As Integer = angka1 + angka2
+
+        lblHasil.Text = "Hasil : " & hasil
+    End Sub
+
+    Private Sub btnKurang_Click(sender As Object, e As EventArgs) Handles btnKurang.Click
+        Dim angka1 As Integer = Convert.ToInt32(txtAngka1.Text)
+        Dim angka2 As Integer = Convert.ToInt32(txtAngka2.Text)
+
+        Dim hasil As Integer = angka1 - angka2
+
+        lblHasil.Text = "Hasil : " & hasil
+    End Sub
+
+    Private Sub btnKali_Click(sender As Object, e As EventArgs) Handles btnKali.Click
+        Dim angka1 As Integer = Convert.ToInt32(txtAngka1.Text)
+        Dim angka2 As Integer = Convert.ToInt32(txtAngka2.Text)
+
+        Dim hasil As Integer = angka1 * angka2
+
+        lblHasil.Text = "Hasil : " & hasil
+    End Sub
+
+    Private Sub btnBagi_Click(sender As Object, e As EventArgs) Handles btnBagi.Click
+        Dim angka1 As Double = Convert.ToDouble(txtAngka1.Text)
+        Dim angka2 As Double = Convert.ToDouble(txtAngka2.Text)
+
+        If angka2 = 0 Then
+            MessageBox.Show("Tidak bisa membagi dengan nol!")
+            Exit Sub
+        End If
+
+        Dim hasil As Double = angka1 / angka2
+
+        lblHasil.Text = "Hasil : " & hasil
+    End Sub
+
+End Class
